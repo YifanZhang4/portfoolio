94d01d3ec0ba508426b68db8e03c91cb66c9d93b
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-06-24 21:17:48 -0400

    badop bomp

---
+++ portfoolio/src/components/SockeyeSalmon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="inTank">
+    <img
+      src="/portfoolio/src/assets/Sockeye_Salmon_Base.png"
+      alt="Sockeye Salmon drawn in a pixel art style"
+      id="salmon"
+    />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+
+let inTank = ref(true)
+
+const swimming = () => {}
+
+onMounted(() => {
+  swimming()
+})
+</script>
+
+<style scoped></style>
---
+++ portfoolio/src/views/Fish.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Tank</h1>
+    <div id="tank"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let fish = ref[]
+</script>
+
+<style scoped>
+#tank {
+  background-color: cornflowerblue;
+  height: 20vh;
+  width: 30vw;
+}
+</style>
