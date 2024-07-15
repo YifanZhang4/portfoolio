34de00e795f02deaf38b0e728c952653b22af7ab
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2024-07-15 09:07:21 -0400

    gayme

---
+++ portfoolio/src/components/game-tab.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-if="open" class="tab-open">
+      <h2>{{ game.name }}</h2>
+      <img :src="game.img" alt="" />
+      <h4>Rating: {{ game.rate }}/10</h4>
+    </div>
+    <div v-if="closed" class="tab-closed">
+      <h2>{{ game.name }}</h2>
+      <h4>{{ game.rate }}/10</h4>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
